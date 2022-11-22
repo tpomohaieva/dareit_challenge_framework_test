bc2655070db95dbd0a38ff1ad6620968383c4e89
Tetiana Pomohaieva <t.pomogaeva@gmail.com>
2022-11-22 23:41:42 +0100

    submit task4 subtask 4

--- test_cases/add_player.py
+++ test_cases/add_player.py
@@ -7,10 +7,10 @@ from pages.dashboard import Dashboard
 from pages.login_page import LoginPage
 from utils.settings import DRIVER_PATH, IMPLICITLY_WAIT
 from selenium.webdriver.chrome.service import Service
+from PIL import Image
 
 
 class TestAddPlayerPage(unittest.TestCase):
-
     @classmethod
     def setUp(self):
         os.chmod(DRIVER_PATH, 755)
@@ -20,7 +20,7 @@ class TestAddPlayerPage(unittest.TestCase):
         self.driver.implicitly_wait(IMPLICITLY_WAIT)
 
     @classmethod
-    def test_add_player_with_valid_data(self): # ST07
+    def test_add_player_with_valid_data(self):  # ST07
         user_login = LoginPage(self.driver)
         user_login.type_in_email("user01@getnada.com")
         user_login.type_in_password("Test-1234")
@@ -29,6 +29,8 @@ class TestAddPlayerPage(unittest.TestCase):
         dashboard.click_on_add_player()
         add_player = AddPlayerPage(self.driver)
         add_player.title_of_the_page()
+        self.driver.save_screenshot("./screenshots/add_player/add-player.png")
+        Image.open("./screenshots/add_player/add-player.png").show()
         add_player.type_in_name("Test")
         add_player.type_in_surname("Testowski")
         add_player.type_in_age("01/01/1990")
@@ -37,6 +39,8 @@ class TestAddPlayerPage(unittest.TestCase):
         add_player.player_added_title_of_the_page()
         add_player.assert_element_text(self.driver, add_player.add_player_form_title_xpath,
                                        add_player.player_added_form_title_text)
+        self.driver.save_screenshot("./screenshots/add_player/player-added.png")
+        Image.open("./screenshots/add_player/player-added.png").show()
 
     @classmethod
     def test_add_player_with_missing_required_data(self):  # ST08
@@ -51,6 +55,8 @@ class TestAddPlayerPage(unittest.TestCase):
         add_player.click_on_the_name_field()
         add_player.click_on_the_submit_button()
         add_player.show_name_required_error()
+        self.driver.save_screenshot("./screenshots/add_player/validation-error.png")
+        Image.open("./screenshots/add_player/validation-error.png").show()
 
     @classmethod
     def test_clear_date_on_add_player_form(self):  # ST09
@@ -67,6 +73,8 @@ class TestAddPlayerPage(unittest.TestCase):
         add_player.type_in_phone("+48123456789")
         add_player.type_in_age("01/01/1990")
         add_player.type_in_main_position("Forward")
+        self.driver.save_screenshot("./screenshots/add_player/form-filled-in.png")
+        Image.open("./screenshots/add_player/form-filled-in.png").show()
         add_player.click_on_the_clear_button()
         add_player.assert_element_text(self.driver, add_player.name_field_xpath, "")
         add_player.assert_element_text(self.driver, add_player.surname_field_xpath, "")
@@ -74,6 +82,8 @@ class TestAddPlayerPage(unittest.TestCase):
         add_player.assert_element_text(self.driver, add_player.age_field_xpath, "")
         add_player.assert_element_text(self.driver, add_player.name_field_xpath, "")
         add_player.assert_element_text(self.driver, add_player.main_position_field_xpath, "")
+        self.driver.save_screenshot("./screenshots/add_player/form-cleared.png")
+        Image.open("./screenshots/add_player/form-cleared.png").show()
 
     @classmethod
     def tearDown(self):
--- test_cases/login_to_the_system.py
+++ test_cases/login_to_the_system.py
@@ -1,5 +1,7 @@
 import os
 import unittest
+
+from PIL import Image
 from selenium import webdriver
 
 from pages.dashboard import Dashboard
@@ -28,6 +30,8 @@ class TestLoginPage(unittest.TestCase):
         user_login.type_in_password("Test-1234")
         user_login.wait_for_button_to_be_clickable(user_login.sign_in_button_xpath)
         user_login.click_on_the_sign_in_button()
+        self.driver.save_screenshot("./screenshots/login_to_the_system/login-form-filled.png")
+        Image.open("./screenshots/login_to_the_system/login-form-filled.png").show()
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_the_page()
 
@@ -43,6 +47,8 @@ class TestLoginPage(unittest.TestCase):
         user_login.wait_for_element_to_be_visible(user_login.invalid_data_caption_xpath)
         user_login.assert_element_text(self.driver, user_login.invalid_data_caption_xpath,
                                        user_login.invalid_password_text)
+        self.driver.save_screenshot("./screenshots/login_to_the_system/invalid-password.png")
+        Image.open("./screenshots/login_to_the_system/invalid-password.png").show()
 
     @classmethod
     def test_log_in_to_the_system_with_missing_login(self): # ST03
@@ -55,6 +61,8 @@ class TestLoginPage(unittest.TestCase):
         user_login.wait_for_element_to_be_visible(user_login.invalid_data_caption_xpath)
         user_login.assert_element_text(self.driver, user_login.invalid_data_caption_xpath,
                                        user_login.invalid_login_text)
+        self.driver.save_screenshot("./screenshots/login_to_the_system/missing-login.png")
+        Image.open("./screenshots/login_to_the_system/missing-login.png").show()
 
     @classmethod
     def test_remind_password_form(self): # ST04
@@ -65,6 +73,8 @@ class TestLoginPage(unittest.TestCase):
         user_login.click_on_the_remind_pw_button()
         remind_pw_page = RemindPasswordPage(self.driver)
         remind_pw_page.title_of_the_page()
+        self.driver.save_screenshot("./screenshots/login_to_the_system/remind-pw.png")
+        Image.open("./screenshots/login_to_the_system/remind-pw.png").show()
 
     @classmethod
     def test_log_out_from_the_system(self):  # ST05
@@ -77,9 +87,13 @@ class TestLoginPage(unittest.TestCase):
         user_login.click_on_the_sign_in_button()
         dashboard_page = Dashboard(self.driver)
         dashboard_page.title_of_the_page()
+        self.driver.save_screenshot("./screenshots/login_to_the_system/dashboard-page.png")
+        Image.open("./screenshots/login_to_the_system/dashboard-page.png").show()
         dashboard_page.click_on_sign_out()
         user_login = LoginPage(self.driver)
         user_login.title_of_the_page()
+        self.driver.save_screenshot("./screenshots/login_to_the_system/signed-out.png")
+        Image.open("./screenshots/login_to_the_system/signed-out.png").show()
 
     @classmethod
     def tearDown(self):
